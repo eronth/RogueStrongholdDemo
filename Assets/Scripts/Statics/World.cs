3f734002ae10b480e2d7689bcda9ce7f9164d7d8
eronth <eronth@gmail.com>
2020-05-04 21:16:22 -0500

    Update static classes.

---
+++ Assets/Scripts/Statics/World.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class World
+{
+    public static Grid Grid 
+        = GameObject.Find("WorldGrid").GetComponent<Grid>();
+}
