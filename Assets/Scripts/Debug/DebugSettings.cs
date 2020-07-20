a2a8dc48cad565f7fd365a73b22cf2f2e884d09f
eronth <eronth@gmail.com>
2020-07-20 16:43:25 -0500

    Debugger improvements

---
+++ Assets/Scripts/Debug/DebugSettings.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DebugSettings
+{
+    public static bool DebugZones { get; private set; } = true;
+    public static bool DebugZoneSurroundings { get; private set; } = true;
+    public static bool DebugPath { get; private set; } = true;
+}
